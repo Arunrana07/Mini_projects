6abb0cdf0af8110e83a615d266c1e64e2712afcc
Arun Kumar <sarunk2r17000@gmail.com>
2026-06-22 18:49:04 +0530

    little bit

--- app.js
+++ app.js
@@ -2,40 +2,40 @@ import readline from 'readline'
 // create a interface to write data aur read data
 
 const rl = readline.createInterface({
-input:process.stdin,   // to read the data
-output:process.stdout   // by help we write
+    input: process.stdin,   // to read the data
+    output: process.stdout   // by help we write
 })
 
 const todo = [];
 
-const showMenu = ()=> {
-    console.log( "\n1 : Add a Task ");
-    console.log( "2 : view task ");
-    console.log( "3 : Exit ");
+const showMenu = () => {
+    console.log("\n1 : Add a Task ");
+    console.log("2 : view task ");
+    console.log("3 : Exit ");
     rl.question("Choose an option : ", handleInput)
-    
+
 }
-const handleInput = (option) =>{
-    if(option === "1"){
-        rl.question("Enter the Task : ",(task) => {
+const handleInput = (option) => {
+    if (option === "1") {
+        rl.question("Enter the Task : ", (task) => {
             todo.push(task);
-        
-        console.log("Task added");
-        showMenu();
+
+            console.log("Task added");
+            showMenu();
         })
     }
-    else if(option === "2"){
+    else if (option === "2") {
         console.log("\n Your Todo lists ");
-        todo.forEach((task , index) => {
+        todo.forEach((task, index) => {
             console.log(`${index + 1}. ${task}`);
         });
         showMenu();
     }
-    else if(option === "3" ){
+    else if (option === "3") {
         console.log("Good Bye");
         rl.close()
     }
-    else{
+    else {
         console.log("Invalid option. please try again");
         showMenu();
     }
